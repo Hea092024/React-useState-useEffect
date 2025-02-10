abaff0361b43e76b425a953722dca3f88e3810d8
Hector Acevedo <HEA092024@gmail.com>
2025-02-10 10:51:49 +0100

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import "./App.css";
 
 export default function App(){
   const [count, setCount] = useState(0);
+  function handleIncrement(){
+    
+
+  }
   return (
     <div>
       <p>{count}</p>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   )
 }
