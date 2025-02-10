58ec61dd0e5e805ea2c56a8d65c3d8e299821756
Hector Acevedo <HEA092024@gmail.com>
2025-02-10 11:25:30 +0100

    added Decrement button with reversed animation

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,25 @@ export default function App() {
   function resetButton() {
     setCount(0);
   }
+
+  function decrement(){
+    setCount(count - 1);
+  }
   return (
     <div>
       <p>{count}</p>
       <button className="btn" onClick={handleIncrement}>
         {" "}
-        <img src={cookie} alt="a picture of a cookie" />{" "}
+        <img src={cookie} alt="a picture of a cookie" />Increment
+      </button>
+
+      <button id="reversedCookie" onClick={decrement}>
+        Decrement
+        <img
+          className="reversedCookieImage"
+          src={cookie}
+          alt="a picture of a cookie"
+        />
       </button>
 
       <button className="btn" onClick={resetButton}>
