6f597148437394118045c1fe1223c19c8f7ef2e3
Hector Acevedo <HEA092024@gmail.com>
2025-02-10 10:01:17 +0100

    added picture of cookie, attached it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import cookieKjeks from "/cookie.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    
       <div className="card">
+        <img src="cookie.png" alt="Image of a Cookie" srcset="" />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-       
       </div>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
