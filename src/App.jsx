085791517de9ba6c294087ad52c6630e0f24d6bd
Hector Acevedo <HEA092024@gmail.com>
2025-02-10 11:08:15 +0100

    reset button added for Alex pleasure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import cookieKjeks from "/cookie.png";
+import cookie from "/cookie.png";
 import "./App.css";
 
-export default function App(){
+export default function App() {
   const [count, setCount] = useState(0);
-  function handleIncrement(){
-    setCount(count + 1)
-
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function resetButton () {
+    setCount(0)
   }
   return (
     <div>
       <p>{count}</p>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleIncrement}>
+        {" "}
+        <img src={cookie} alt="a picture of a cookie" />{" "}
+      </button>
+
+      <button onClick={resetButton}>Reset</button>
     </div>
-  )
+  );
 }
