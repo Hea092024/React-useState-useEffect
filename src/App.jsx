bd2755e216f03ab9b721d8f117e28d7e102b8fa2
Hector Acevedo <HEA092024@gmail.com>
2025-02-10 10:46:44 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,13 @@ import cookieKjeks from "/cookie.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+const [count, setCount] = useState(0)
+  }
   return (
-    <>
-      <div className="card">
-        <img src="cookie.png" alt="Image of a Cookie" />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div>
+      
+    </div>
   );
-}
+
 
 export default App;
