5011a7f4be723d7a8019808fdd4106381697b9f1
Hector Acevedo <HEA092024@gmail.com>
2025-02-11 10:03:35 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import CookieClicker from "./components/CookieClicker.jsx"
-
+import ArrayState from "./components/CatFacts.jsx"
 function App() {
   return (
     <>
       <CookieClicker />
+      <ArrayState />
     </>
   );
 }
