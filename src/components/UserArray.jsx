edd437af09d725b0c2bea50977566756b3975eed
Hector Acevedo <HEA092024@gmail.com>
2025-02-11 13:26:26 +0100

    .

--- src/components/UserArray.jsx
+++ src/components/UserArray.jsx
@@ -1,25 +1,57 @@
 import { useState } from "react";
 
-const mockData = [
-    { username: 'Ola Normann', email: 'ola.normann@norge.no'},
-    { username: 'Torleif', email: 'torleif@kodehode.no' },
-    { username: 'Jan Egil', email: 'jan.egil@kodehode.no' },
-    { username: 'Sander', email: 'sander@kodehode.no' },
-]
+const UserArray = () => {
+  const mockData = [
+    { username: "Ola Normann", email: "ola.normann@norge.no" },
+    { username: "Torleif", email: "torleif@kodehode.no" },
+    { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+    { username: "Sander", email: "sander@kodehode.no" },
+  ];
 
-  const handleField = () => {
-    const newString = document.getElementById("text-field").value;
-    document.getElementById("text-field").value = "";
-    setArray([...array, , newString]);
-  
+  const [user, setUser] = useState(mockData);
+
+  const userChange = (e) => {
+    setUser((u) =>
+      u.map((item, index) =>
+        index === 0 ? { ...item, author: e.target.value } : item
+      )
+    );
+  };
+
+  const emailChange = (e) => {
+    setUser((u) =>
+      u.map((item, index) =>
+        index === 0 ? { ...item, email: e.target.value } : item
+      )
+    );
+  };
 
   return (
     <>
-      <input type="text" id="text-field" placeholder="insert text here" />
-      <button onClick={() => handleField()}>submit</button>
-      {array.map((inst, index) => (
-        <p key={index}>{inst}</p>
-      ))}
+      <div>
+        {user.map((user, index) => (
+          <div key={index}>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          id="author"
+          value={user[0].author || ""}
+          onChange={userChange}
+        />
+        <input
+          type="text"
+          id="tittle"
+          value={user[0].tittle || ""}
+          onChange={emailChange}
+        />
+        <input type="text" id="year" value={user[0].year || ""} />
+      </div>
     </>
   );
 };
