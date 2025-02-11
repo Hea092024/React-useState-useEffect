cfbd66d0f0785665a7846ff8983a3b03dab615af
Hector Acevedo <HEA092024@gmail.com>
2025-02-11 10:54:50 +0100

    .

--- src/components/ObjectState.jsx
+++ src/components/ObjectState.jsx
@@ -11,27 +11,41 @@ const authorChange =(e) => {
     setBook ((b) => ({ ... b, author: e.target.value}))
 }
 
-const tittleChange =() => {
-
+const tittleChange =(e) => {
+setBook((b) => ({ ...b, tittle: e.target.value }));
 }
 
-const yearChange =() => {
-
+const yearChange =(e) => {
+setBook((b) => ({ ...b, year: e.target.value }));
 }
 
     return (
       <>
-
-      
         <div>
           <p>{book.author}</p>
           <p>{book.tittle}</p>
           <p>{book.year}</p>
         </div>
+
         <div>
-            <input type="text" id="author" value={book.author} />
-            <input type=" id" id="tittle" value={book.tittle} />
-            <input type="text" id="year" value={book.year} />  
+          <input
+            type="text"
+            id="author"
+            value={book.author}
+            onChange={authorChange}
+          />
+          <input
+            type=" id"
+            id="tittle"
+            value={book.tittle}
+            onChange={tittleChange}
+          />
+          <input
+            type="text"
+            id="year"
+            value={book.year}
+            onChange={yearChange}
+          />
         </div>
       </>
     );
