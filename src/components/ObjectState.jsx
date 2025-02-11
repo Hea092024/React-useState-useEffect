46e0edf352d187dafeb007c946681771be5320b9
Hector Acevedo <HEA092024@gmail.com>
2025-02-11 10:43:34 +0100

    .

--- src/components/ObjectState.jsx
+++ src/components/ObjectState.jsx
@@ -7,12 +7,19 @@ tittle: "Dungeon Crawler Carl",
 year : 2020,
 })
     return (
-        <>
-        <p>{book.author}</p>
-        <p>{book.tittle}</p>
-        <p>{book.year}</p>
-        </>
-    )
+      <>
+        <div>
+          <p>{book.author}</p>
+          <p>{book.tittle}</p>
+          <p>{book.year}</p>
+        </div>
+        <div>
+            <input type="text" id="author" value={book.author} />
+            <input type=" id" id="tittle" value={book.tittle} />
+            <input type="text" id="year" value={book.year} />  
+        </div>
+      </>
+    );
     
 }
 export default ObjectState
