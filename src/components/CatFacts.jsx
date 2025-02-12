98ddd026e59854ac10135286e73b327b926552f6
Hector Acevedo <HEA092024@gmail.com>
2025-02-12 18:23:29 +0100

    .

--- src/components/CatFacts.jsx
+++ src/components/CatFacts.jsx
@@ -1,37 +1,32 @@
 import { useState, useEffect } from "react";
+
 export default function CatFacts() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [data, setData] = useState (null)
-    const [loading, setLoading] = useState (true)
-    const [error, setError] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("https://catfact.ninja/facts?limit=5");
+        if (!response.ok)
+          throw new Error(`HTTP error. Status ${response.status}`);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() =>{
-        const fetchData = async () => {
-setLoading(true)
-setError(null)
-try{
-    const response = await fetch ("https://catfact.ninja/facts?limit=5")
-    if (!response.ok)
-        throw new Error (`HTTP error. Status ${response.status}`)
+  return (
+    <div>
+      <h1>Hei</h1>
+    </div>
+  );
 }
-const result = await response.json()
-setData(result)
-        }
-        catch (error) {
-            setError {error.message}
-        }
-        finally { 
-            setLoading(false);
-        }
-
-    }, [])
-fetchData ()
-
-    return(
-
-
-        <div>
-            <h1>Hei</h1>
-        </div>
-    )
-};
