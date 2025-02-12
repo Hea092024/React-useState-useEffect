a69173266394288bbc8b8ffa35454767ef97f441
Hector Acevedo <HEA092024@gmail.com>
2025-02-12 18:07:39 +0100

    .

--- src/components/CatFacts.jsx
+++ src/components/CatFacts.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 export default function CatFacts() {
+
+    const [data, setData] = useState (null)
+    const [loading, setLoading] = useState (true)
+    const [error, settError] = useState(null)
+
+}
     return(
 
-        
+
         <div>
             <h1>Hei</h1>
         </div>
