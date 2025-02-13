b232686e387d77ec0151102854df37d3efa61aa6
Hector Acevedo <HEA092024@gmail.com>
2025-02-13 09:30:08 +0100

    Fix base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-useState-useEffect/",
+  base: "/React-useState-useEffect/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
