21aa66c90d3ffb3bf3eb8dee62245f78c658f38e
Hector Acevedo <HEA092024@gmail.com>
2025-02-13 09:11:42 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-useState-useEffect/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
